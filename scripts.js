890db8cd300639b6286fb22bf822fd393737acbc
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-07-26 23:38:25 +0400

    calculator works
    1. modified html - changed textContent for some of the buttons: e.g. 'x' substituted with '*', since I am using textConten to match the mathematical operation (at least for now)
    2. added fillExpression function. This function is where the whole flow takes place.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,28 @@
-let number1, number2, operator
+let number1 = ''
+let number2 = ''
+let operator = ''
+const digitButtons = document.querySelectorAll('.btn')
+
+digitButtons.forEach( button => {
+    button.addEventListener( 'click', event => {
+        fillExpression( event.target.textContent )
+    });
+});
+
+function fillExpression( input ) {
+    if( '+-*/'.includes( input ) ) {
+        operator = input
+        console.log(`operator: ${operator}`)
+    } else if( input === '=' ) {
+        console.log(`result = ${operate( +number1, +number2, operator )}`)
+    } else if( operator === '') {
+        number1 += input
+        console.log(`number1: ${number1}`)
+    } else if( operator !== '') {
+        number2 += input
+        console.log(`number2: ${number2}`)
+    }
+}
 
 function add( a, b ) {
     return a + b;
@@ -19,13 +43,13 @@ function divide( a, b ) {
 
 function operate( num1, num2, op ) {
     switch( op ) {
-        case '+': add( num1, num2 )
+        case '+': return add( num1, num2 )
         break;
-        case '-': subtract( num1, num2 )
+        case '-': return subtract( num1, num2 )
         break;
-        case '*': multiply( num1, num2 )
+        case '*': return multiply( num1, num2 )
         break;
-        case '/': divide( num1, num2 )
+        case '/': return divide( num1, num2 )
         break;
     }
 }
