c6f51ba631f387921efd1acf3eb370c12549ad14
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-08-01 13:56:49 +0400

    'Enter' bug is fixed
    Problem: pressing 'Enter' would trigger click on the last pressed button
    clicked with mouse.
    Solution: using event.preventDefault() solved the issue

--- scripts.js
+++ scripts.js
@@ -8,51 +8,54 @@ const displayResultEl = document.getElementById('display-result')
 displayLiveEl.textContent = '0'
 
 digitButtons.forEach( button => {
-    button.addEventListener( 'click', event => {
-        fillExpression( event.target.textContent )
-    });
+    button.addEventListener( 'click', event => fillExpression( event.target.textContent ))
 });
 
 document.onkeydown = event => {
+    if( event.key === 'Enter') {
+        event.preventDefault()
+    }
     fillExpression( event.key )
-};
+}
+
+const exprLen = () => expression.length
 
 function fillExpression( input ) {
-    switch( input ) {
-        case 'C': expression = []
+    console.log('key: ' + input)
+    switch( true ) {
+        case input === 'C': expression = []
         break;
-        case '⇦': case 'Backspace': inputBackspace( input )
+        case ['⇦', 'Backspace'].includes( input ): inputBackspace( input )
         break;
-        case '+/-': inputSign( input )
+        case input === '+/-': inputSign( input )
         break;
-        case '.': inputDot( input )
+        case input === '.': inputDot( input )
         break;
-        case '+': case '-': case '*': case '/': inputOperation( input )
+        case '+-*/'.includes( input ): inputOperation( input )
         break;
-        case '=': case 'Enter': inputEnter( input )
+        case ['=', 'Enter'].includes( input ): inputEnter( input )
         break;
-        case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': {
-            inputDigit(input)
-            break;
-        }        
+        case '0123456789'.includes( input ): inputDigit(input)            
+        break;      
+        default: break;  
     }
     console.log(expression)
     render()
 }
 
 function inputDigit( input ) { // 0,1,2,3,4,5,6,7,8,9
-    if( expression.length % 2 === 0 ) {
+    if( exprLen() % 2 === 0 ) {
         expression.push(input)
     } else if( isResult === true ) {
         expression[0] = input
         isResult = false
     } else {
-        expression[ expression.length-1 ] += input
+        expression[ exprLen()-1 ] += input
     }
 }
 
 function inputBackspace( input ) {
-    if( expression.length > 0 ) {
+    if( exprLen() > 0 ) {
         let last = expression.pop()
         if( last.length > 1 ) {
             expression.push(last.slice(0, -1))
@@ -61,29 +64,29 @@ function inputBackspace( input ) {
 }
 
 function inputSign( input ) { //   +/-
-    if( expression.length % 2 !== 0 ) {
-        let number = +expression[ expression.length - 1 ]  
-        expression[ expression.length - 1 ] = (number * (-1)).toString()
+    if( exprLen() % 2 !== 0 ) {
+        let number = +expression[ exprLen() - 1 ]  
+        expression[ exprLen() - 1 ] = (number * (-1)).toString()
     }
 }
 
 function inputDot( input ) {
-    if( expression.length % 2 === 0 ) {
+    if( exprLen() % 2 === 0 ) {
         expression.push( input )            
     } else {
-        if( !expression[ expression.length - 1 ].includes('.') ) {
-            expression[ expression.length - 1 ] += '.'
+        if( !expression[ exprLen() - 1 ].includes('.') ) {
+            expression[ exprLen() - 1 ] += '.'
         }
     }
 }
 
-function inputOperation( input ) {
+function inputOperation( input ) { //+,-,*,/
     isResult = false
-    if( expression.length === 0 && input === '-' ) {
+    if( exprLen() === 0 && input === '-' ) {
         expression.push( input )
-    } else if( expression.length > 0 && expression.length % 2 === 0 && input !== '-'
-                || expression[ expression.length - 1 ] === '+' && input === '-' ) {
-        expression[ expression.length - 1 ] = input 
+    } else if( exprLen() > 0 && exprLen() % 2 === 0 && input !== '-'
+               || expression[ exprLen() - 1 ] === '+' && input === '-' ) {
+        expression[ exprLen() - 1 ] = input 
     } 
     else {
         expression.push( input )
@@ -91,7 +94,7 @@ function inputOperation( input ) {
 }
 
 function inputEnter( input ) {
-    if( expression.length > 1 && expression.length % 2 !== 0 ) {
+    if( exprLen() > 1 && exprLen() % 2 !== 0 ) {
         expression[0] = calcExpression().toString()
         expression.splice(1)
         isResult = true
@@ -100,7 +103,7 @@ function inputEnter( input ) {
 
 function calcExpression() {
     let result = expression[0]
-    for( let i = 2; i < expression.length; i += 2 ) {
+    for( let i = 2; i < exprLen(); i += 2 ) {
         result = operate( +result, +expression[i], expression[i-1])
     }
     return Number(Math.round(result + 'e7') + "e-7")
@@ -117,10 +120,10 @@ function render() {
         displayLiveEl.textContent = exprStr
     }
 
-    if( expression.length >= 3 ) {
+    if( exprLen() >= 3 ) {
         let result = calcExpression()
         if( isNaN(result) ) {
-            displayResultEl.textContent = ''
+            displayResultEl.textContent = 'NaN'
         } else {
             displayResultEl.textContent = result
         }
@@ -132,24 +135,15 @@ function render() {
 function calcDisplayLiveFontSize( str ) {
     let strLen = str.length
     switch( true ) {
-        case strLen <= 7: 
-            return '60px';
-        case strLen > 14: 
-            return '31px';
-        case strLen > 13: 
-            return '32px';
-        case strLen > 12: 
-            return '35px';
-        case strLen > 11: 
-            return '38px';
-        case strLen > 10: 
-            return '41px';  
-        case strLen > 9:
-            return '45px';  
-        case strLen > 8: 
-            return '50px';  
-        case strLen > 7:
-            return '55px';          
+        case strLen <= 7: return '60px';
+        case strLen > 14: return '31px';
+        case strLen > 13: return '32px';
+        case strLen > 12: return '35px';
+        case strLen > 11: return '38px';
+        case strLen > 10: return '41px';  
+        case strLen > 9:  return '45px';  
+        case strLen > 8:  return '50px';  
+        case strLen > 7:  return '55px';          
     }       
 }
 
@@ -173,13 +167,9 @@ function divide( a, b ) {
 function operate( num1, num2, op ) {
     switch( op ) {
         case '+': return add( num1, num2 )
-        break;
         case '-': return subtract( num1, num2 )
-        break;
         case '*': return multiply( num1, num2 )
-        break;
         case '/': return divide( num1, num2 )
-        break;
     }
 }
 
